cee3a9c43eeac791b4b5ae8bf369821938cc22ce
kuroshiro <emsinos1902@gmail.com>
2023-07-03 21:12:53 +0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
+import { PostCard, Slider, TopicCard } from './components';
 import { Footer, Header } from './public';
 
 function App() {
     return (
-        <div id="dark">
+        <div id="dark" className='mode'>
             <Header />
-            <div id="testScroll" style={{ height: '2000px' }}></div>
-            <Footer />
+            <div id='container'>
+                <div id="testScroll" style={{ height: '2000px' }}>
+                    <div style={{height: '180px', width:'50%', padding: '1rem'}}>
+                        <Slider slides={[<TopicCard />,<TopicCard />,<TopicCard />,<TopicCard />,<TopicCard />,<TopicCard />, ]} />
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </div>
     );
 }
